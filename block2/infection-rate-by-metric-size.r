a2f9a6a64f43b87c29505865396721544f92a96a
Felix Dekker <felix@fwdekker.com>
2018-09-23 17:42:27 +0200

    Add code for second metric

---
+++ block2/infection-rate-by-metric-size.r
@@ -0,0 +1,29 @@
+library(plyr)
+
+# Import
+spamhaus <- read.csv("spamhaus.csv")
+
+# Preprocess
+spamhaus$Diagnostic <- as.character(spamhaus$Diagnostic)
+spamhaus <- spamhaus[(startsWith(spamhaus$Diagnostic, "BOT")),]
+spamhaus <- spamhaus[(spamhaus$ASN != "AS?"),]
+
+# IPs per allocation
+ipsPerAllocation <- count(spamhaus, c("Country", "ASN", "Allocation"))
+colnames(ipsPerAllocation)[4] <- "IPs"
+
+# Allocation block size
+ipsPerAllocation$AllocationSize <- regmatches(ipsPerAllocation$Allocation, regexpr("/.*", ipsPerAllocation$Allocation))
+ipsPerAllocation$AllocationSize <- regmatches(ipsPerAllocation$AllocationSize, regexpr("[^/]+", ipsPerAllocation$AllocationSize))
+ipsPerAllocation$AllocationSize <- as.numeric(ipsPerAllocation$AllocationSize)
+ipsPerAllocation$AllocationSize <- 2^(32 - ipsPerAllocation$AllocationSize)
+ipsPerAllocation$Allocation <- NULL
+
+# Sum size and IPs per ASN
+asnRates <- aggregate(. ~ Country+ASN, ipsPerAllocation, sum)
+asnRates$Percentage <- asnRates$IPs / asnRates$AllocationSize
+asnRates$ASN <- NULL
+asnRates$IPs <- NULL
+
+# Plot
+plot(asnRates$AllocationSize, asnRates$Percentage, log="x", xlab="Number of allocations", ylab="Infection rate", main="AS infection rate by size")
