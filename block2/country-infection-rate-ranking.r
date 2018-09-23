c859eaff4d1b2c1e5ce4931f476f667df29f5ae1
Felix Dekker <felix@fwdekker.com>
2018-09-23 21:14:25 +0200

    Improve readability

--- block2/country-infection-rate-ranking.r
+++ block2/country-infection-rate-ranking.r
@@ -1,15 +1,22 @@
-# Import/preprocess
+# Spamhaus
 spamhaus <- read.csv("spamhaus.csv")
+
+## Sanitise
 spamhaus$Diagnostic <- as.character(spamhaus$Diagnostic)
 spamhaus <- spamhaus[(startsWith(spamhaus$Diagnostic, "BOT")),]
 spamhaus <- spamhaus[(spamhaus$Country != "" & spamhaus$Country != "??"),]
 
+
+# Countries
 countries <- read.csv("countries.csv")
+
+## Sanitise
 countries <- countries[c("Symbol", "InternetUsers")]
 colnames(countries)[1] <- "Country"
 countries <- countries[(countries$Country != "--" & countries$InternetUsers != "n/a"),]
 countries$InternetUsers <- as.numeric(as.character(countries$InternetUsers))
 countries <- countries[(countries$InternetUsers > 1000),]
+
 
 # Infections per country
 infectionsPerCountry <- count(spamhaus, c("Country"))
