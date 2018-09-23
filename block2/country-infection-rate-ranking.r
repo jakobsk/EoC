b8930e732b192b8cb7d6550ca52beed39b2d4946
Felix Dekker <felix@fwdekker.com>
2018-09-23 18:13:32 +0200

    Add incomplete script for first metric

---
+++ block2/country-infection-rate-ranking.r
@@ -0,0 +1,27 @@
+# Import
+spamhaus <- read.csv("spamhaus.csv")
+countries <- read.csv("countries.csv")
+
+# Preprocess
+spamhaus <- spamhaus[(startsWith(spamhaus$Diagnostic, "BOT")),]
+spamhaus <- spamhaus[(spamhaus$Country != "" & spamhaus$Country != "??"),]
+spamhaus$Diagnostic <- as.character(spamhaus$Diagnostic)
+
+countries <- countries[c("Symbol", "Internet.users")]
+colnames(countries) <- c("Country", "InternetUsers")
+countries <- countries[(countries$Country != "--" & countries$InternetUsers != "n/a"),]
+countries$InternetUsers <- as.numeric(countries$InternetUsers)
+
+# Infections per country
+infectionsPerCountry <- count(spamhaus, c("Country"))
+colnames(infectionsPerCountry)[2] <- "Infections"
+infectionsPerCountry$Infections <- as.numeric(infectionsPerCountry$Infections)
+infectionsPerCountry <- merge(infectionsPerCountry, countries, by="Country")
+
+# Infection rate
+infectionsPerCountry$InfectionRate <- infectionsPerCountry$Infections / infectionsPerCountry$InternetUsers
+infectionsPerCountry$Infections <- NULL
+infectionsPerCountry$InternetUsers <- NULL
+
+# Plot
+## TODO
