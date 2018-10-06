51a7e22a1d253cec7f25dd2bdd69bd293c8bd0a6
Felix Dekker <felix@fwdekker.com>
2018-10-06 15:38:41 +0200

    Use data frames

--- block3/impact-cost-distribution.r
+++ block3/impact-cost-distribution.r
@@ -1,9 +1,11 @@
 # Makes plot of the distribution of how much an gamut infection is going to cost the average person 
-cost <- seq(from=0, to=22, by=0.25)
-density <- dnorm(cost, mean=11, sd=2)
-plot(cost, density, type="l", ylab = "Density", xlab = "Cost in $", main="Distribution of costs being on a blocklist")
+impactPerson = data.frame()[1:89, ]
+impactPerson$cost = seq(from=0, to=22, by=0.25);
+impactPerson$density = dnorm(impactPerson$cost, mean=11, sd=2);
+plot(impactPerson$cost, impactPerson$density, type="l", ylab = "Density", xlab = "Cost in $", main="Distribution of costs being on a blocklist")
 
 # Same plot, but now applied to the 64M internet users in vietnam
-cost <- seq(from=0, to=1200, by=1)
-density <- dnorm(cost, mean=704, sd=128)
-plot(cost, density, type="l", ylab = "Density", xlab = "Cost in million $", main="Impact distribution for Vietnam")
+impactVN = data.frame()[1:1201, ];
+impactVN$cost = seq(from=0, to=1200, by=1);
+impactVN$density = dnorm(impactVN$cost, mean=704, sd=128);
+plot(impactVN$cost, impactVN$density, type="l", ylab = "Density", xlab = "Cost in million $", main="Impact distribution for Vietnam")
