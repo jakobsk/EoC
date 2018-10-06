f6712604d41994c1d0545ea401f15844044c3ebd
Felix Dekker <felix@fwdekker.com>
2018-10-06 15:25:31 +0200

    Improve variable name readability

--- block3/impact-cost-distribution.r
+++ block3/impact-cost-distribution.r
@@ -1,9 +1,9 @@
 # Makes plot of the distribution of how much an gamut infection is going to cost the average person 
-x <- seq(from=0, to=22, by=0.25)
-dens <- dnorm(x, mean=11, sd=2)
-plot(x, dens, type="l", ylab = "Density", xlab = "Cost in $", main="Distribution of costs being on a blocklist")
+cost <- seq(from=0, to=22, by=0.25)
+density <- dnorm(cost, mean=11, sd=2)
+plot(cost, density, type="l", ylab = "Density", xlab = "Cost in $", main="Distribution of costs being on a blocklist")
 
 # Same plot, but now applied to the 64M internet users in vietnam
-x <- seq(from=0, to=1200, by=1)
-dens <- dnorm(x, mean=704, sd=128)
-plot(x, dens, type="l", ylab = "Density", xlab = "Cost in million $", main="Impact distribution for Vietnam")
+cost <- seq(from=0, to=1200, by=1)
+density <- dnorm(cost, mean=704, sd=128)
+plot(cost, density, type="l", ylab = "Density", xlab = "Cost in million $", main="Impact distribution for Vietnam")
