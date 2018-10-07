56477075a35b244c93997cf84b3cf848516c7b29
Felix Dekker <felix@fwdekker.com>
2018-10-07 23:20:57 +0200

    Add risk distribution calculation

---
+++ block3/risk-distribution.r
@@ -0,0 +1,7 @@
+# Run this after running `likelihood-distribution` and `impact-distribution`
+
+risk <- sample(impactVN$cost, 10000, prob=impactVN$density, replace=TRUE) * sample(likelihoodVN$infections, 10000, prob=likelihoodVN$density, replace=TRUE)
+plot(density(risk),
+	 main="Expected risk of infection",
+	 xlab="Cost",
+	 ylab="Density")
