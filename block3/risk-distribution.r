682afb0703f6809b117935ab5e93c6ed3745e055
Felix Dekker <felix@fwdekker.com>
2018-10-08 10:57:40 +0200

    Improve code

--- block3/risk-distribution.r
+++ block3/risk-distribution.r
@@ -1,6 +1,6 @@
 # Run this after running `likelihood-distribution` and `impact-distribution`
 
-risk <- sample(impactVN$cost, 10000, prob=impactVN$density, replace=TRUE) * sample(likelihoodVN$infections, 10000, prob=likelihoodVN$density, replace=TRUE)
+risk <- sample(impactVN$cost, 100000, prob=impactVN$density, replace=TRUE) * sample(likelihoodVN$infections, 100000, prob=likelihoodVN$density, replace=TRUE)
 plot(density(risk),
 	 main="Expected risk of infection",
 	 xlab="Cost in million $",
--- block3/rosi.r
+++ block3/rosi.r
@@ -1,10 +1,10 @@
 # Run this after running `risk`
 
-riskMitigated <- 0.1575
+riskMitigated <- 0.99
 solutionCost <- 112
 
 rosi <- risk * riskMitigated
 rosi <- rosi - solutionCost
 rosi <- rosi / solutionCost
 
-plot(density(rosi))
+plot(ecdf(rosi))
