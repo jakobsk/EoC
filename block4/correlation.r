29462a599b1d456f2b5722b6f33721f770f555bb
Felix Dekker <felix@fwdekker.com>
2018-10-14 21:06:01 +0200

    Read all CSVs for correlation

--- block4/correlation.r
+++ block4/correlation.r
@@ -2,6 +2,8 @@ library(plyr)
 
 fton <- function(f) suppressWarnings(as.numeric(as.character(f)))
 cor2 <- function(a, b) c(cor(a, b, method="p"), cor(a, b, method="k"), cor(a, b, method="s"))
+sym_diff <- function(a, b) setdiff(union(a, b), intersect(a, b))
+set_diff <- function(a, b) setdiff(union(a, b), b)
 
 # Preprocess
 ## Spamhaus
@@ -12,9 +14,38 @@ spamhaus <- spamhaus[(complete.cases(spamhaus)),]
 spamhaus <- spamhaus[(spamhaus$Diagnostic == "BOT gamut"),]
 spamhaus <- spamhaus[(spamhaus$Country != "??"),]
 
+## Computers per capita
+cpp <- read.csv("cpp.csv")
+cpp$Computers.per.Capita <- fton(cpp$Computers.per.Capita)
+
 ## GDP per capita
 gdppc <- read.csv("gdppc.csv")
 gdppc$GDP.per.capita <- fton(gdppc$GDP.per.capita)
+
+## Global Cybersecurity Index
+gci <- read.csv("gci.csv")
+gci$GCI <- fton(gci$GCI)
+
+## IDI
+idi <- read.csv("idi.csv")
+idi$IDI <- fton(idi$IDI)
+
+## Median income
+mi <- read.csv("medianincome.csv")
+mi$Median.household.income <- fton(mi$Median.household.income)
+mi$Median.per.capita.income <- fton(mi$Median.per.capita.income)
+
+## Papers per country
+ppc <- read.csv("ppc.csv")
+ppc$Papers <- fton(ppc$Papers)
+ppc$Computer.Science.papers <- fton(ppc$Computer.Science.papers)
+ppc$Computer.Science.papers.ratio <- ppc$Papers / ppc$Computer.Science.papers
+ppc$Papers <- NULL
+ppc$Computer.Science.papers <- NULL
+
+## Technology index
+ti <- read.csv("techindex.csv")
+ti$Technology.Index <- fton(ti$Technology.Index)
 
 ## Countries
 countries <- read.csv("countries.csv")
@@ -35,7 +66,7 @@ colnames(infections) <- c("Symbol", "InfectionCount")
 
 
 # Merge datasets
-countries <- merge(countries, gdppc, by="Country")
+countries <- merge(countries, mi, by="Symbol")
 countries <- merge(countries, infections, by="Symbol")
 
 # Derive
@@ -44,4 +75,4 @@ countries$InfectionCount <- NULL
 
 
 # Find correlation
-cor <- cor2(countries$InfectionRate, countries$GDP.per.capita)
+cor2(countries$InfectionRate, countries$Median.per.capita.income)
